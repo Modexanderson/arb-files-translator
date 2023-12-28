4e11ad7ffc3e51697497bcc8ca82fa8f0949b665
Modexanderson <mordecai.a.d@gmail.com>
2023-12-28 08:40:06 +0100

    process

--- main.py
+++ main.py
@@ -1,35 +1,84 @@
-from googletrans import Translator
+# from googletrans import Translator
 
+
+# def rreplace(s, old, new, occurrence):
+
+#     li = s.rsplit(old, occurrence)
+
+#     return new.join(li)
+
+
+# if __name__ == "__main__":
+#     inputLocale = 'en'
+#     outputLocales = ['nl']
+#     # ['ar', 'bn', 'cs', 'de', 'es', 'fr', 'he', 'hi',
+#     #                  'id', 'it', 'ja', 'pt', 'ru', 'ta', 'tr', 'uk', 'ur', 'zh-cn']
+
+#     translator = Translator()
+#     file = open("input.arb", "r", encoding="ISO-8859-1")
+
+#     translations = {}
+
+#     for line in file:
+#         # Ignore empty lines and brackets
+#         if (line == '{\n' or line == '}' or line == ''):
+#             continue
+
+#          # Skip lines that don't have a colon and a space
+#         if ': ' not in line:
+#             continue
+
+#         # Ignore lines starting with "@"
+#         # if line.strip().startswith('@'):
+#         #     continue
+
+#         toTranslate = line.split('": ')[1][1:]
+#         toTranslate = toTranslate.split('"')[0]
+
+#         print(toTranslate)
+
+#         for supportedLocale in outputLocales:
+#             translation = translator.translate(
+#                 toTranslate, src=inputLocale, dest=supportedLocale, timeout=300)
+#             newLine = rreplace(line, toTranslate, translation.text, 1)
+
+#             if (supportedLocale in translations.keys()):
+#                 translations[supportedLocale].append(newLine)
+#             else:
+#                 translations[supportedLocale] = [newLine]
+
+#     for key in translations.keys():
+#         with open("result/app_" + key + ".arb", "w", encoding='utf-8') as f:
+#             f.write('{\n')
+#             for line in translations[key]:
+#                 f.write(line)
+#             f.write('}')
+#             f.close()
+
+from googletrans import Translator
+import time
 
 def rreplace(s, old, new, occurrence):
-
     li = s.rsplit(old, occurrence)
-
     return new.join(li)
-
 
 if __name__ == "__main__":
     inputLocale = 'en'
     outputLocales = ['ar', 'bn', 'cs', 'de', 'es', 'fr', 'he', 'hi',
-                     'id', 'it', 'ja', 'pt', 'ru', 'ta', 'tr', 'uk', 'ur', 'zh-cn']
+                     'id', 'it', 'ja', 'nl', 'pt', 'ru', 'ta', 'tr', 'uk', 'ur', 'zh-cn']
 
     translator = Translator()
+
     file = open("input.arb", "r", encoding="ISO-8859-1")
 
     translations = {}
 
     for line in file:
-        # Ignore empty lines and brackets
-        if (line == '{\n' or line == '}' or line == ''):
+        if line == '{\n' or line == '}' or line == '':
             continue
 
-         # Skip lines that don't have a colon and a space
         if ': ' not in line:
             continue
-
-        # Ignore lines starting with "@"
-        # if line.strip().startswith('@'):
-        #     continue
 
         toTranslate = line.split('": ')[1][1:]
         toTranslate = toTranslate.split('"')[0]
@@ -37,11 +86,19 @@ if __name__ == "__main__":
         print(toTranslate)
 
         for supportedLocale in outputLocales:
-            translation = translator.translate(
-                toTranslate, src=inputLocale, dest=supportedLocale)
+            max_retries = 3
+            for retry in range(max_retries):
+                try:
+                    translation = translator.translate(
+                        toTranslate, src=inputLocale, dest=supportedLocale, timeout=30)
+                    break  # Break out of the loop if successful
+                except Exception as e:
+                    print(f"Translation error (retry {retry + 1}/{max_retries}): {e}")
+                    time.sleep(2)  # Wait for 2 seconds before retrying
+
             newLine = rreplace(line, toTranslate, translation.text, 1)
 
-            if (supportedLocale in translations.keys()):
+            if supportedLocale in translations.keys():
                 translations[supportedLocale].append(newLine)
             else:
                 translations[supportedLocale] = [newLine]
